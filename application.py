9189b3335e6937d6cf9a7a85005a9665c1e66f26
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-10-21 18:50:10 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,16 +1,23 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 application = Flask(__name__)
+api = Api(application)
 
-@application.route("/", methods=['GET', 'POST'])
-def index():
-    return "<h1>This is OneUtils Flask Env. API</h1>"
+# @application.route("/", methods=['GET', 'POST'])
+# def index():
+#     return "<h1>This is OneUtils Flask Env. API</h1>"
 
-@application.route("/another", methods=['GET', 'POST'])
-def another():
-    return "<h1>This is OneUtils Another Page</h1>"
+# @application.route("/another", methods=['GET', 'POST'])
+# def another():
+#     return "<h1>This is OneUtils Another Page</h1>"
+class Index(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(Index, '/')
 
 
 
 if __name__ == "__main__":
-    application.run()
+    application.run(debug=True)
