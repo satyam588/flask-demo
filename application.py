1ea96d8af942f37582d42ced503e3989a2a2ed4e
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-10-21 18:43:16 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@application.route("/", methods=['GET', 'POST'])
 def index():
     return "<h1>This is OneUtils Flask Env. API</h1>"
 
 
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
