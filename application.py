4054fe9c50a207dd65ab3eec98b8bd289e81c17b
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-10-21 18:45:38 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,6 +6,10 @@ application = Flask(__name__)
 def index():
     return "<h1>This is OneUtils Flask Env. API</h1>"
 
+@application.route("/another", methods=['GET', 'POST'])
+def another():
+    return "<h1>This is OneUtils Another Page</h1>"
+
 
 
 if __name__ == "__main__":
