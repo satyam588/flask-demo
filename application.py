d90353bf8f22e09c82a17e029d5241c5ac1b8f76
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-11-01 01:50:25 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,5 +1,9 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+import os
+import random
+import string
+import magic
 
 application = Flask(__name__)
 api = Api(application)
@@ -10,8 +14,40 @@ class Index(Resource):
         return {'hello': 'Get Method'}
 
     def post(self):
-        json_request = request.get_json()
-        return json_request
+        file = request.files["image"]
+        if file.filename != '':
+            fileExtention = file.filename.split('.')
+            fileExtention = fileExtention[-1]
+
+            # Save Path
+            savePath = "uploads/" + \
+                ''.join(random.choice(string.ascii_lowercase + string.digits)
+                        for _ in range(10))+"."+fileExtention
+            file.save(savePath)
+            # Get Size in byte
+            size = os.stat(savePath).st_size
+
+            # Get Mime-type
+            mimeType = magic.from_file(savePath, mime=True)
+
+            if (size < 2000000) and ('image' in mimeType):
+                response = {
+                    'filename': file.filename,
+                    'size': size,
+                    'extention': fileExtention,
+                    'save_path': savePath,
+                    'mimeitype': mimeType
+                }
+                return response
+            else:
+                if os.path.exists(savePath):
+                    os.remove(savePath)
+                response = {
+                    'message': 'Only Image and Less then 2MB file allowed!',
+                    'status': 0,
+                    'data': []
+                }
+                return response
 
 
 class Param(Resource):
