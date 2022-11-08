4d1d745abc84292c3224c23520a260ccb9a741e6
SATYAM SUNDARAM <sundaramsatyam12@gmail.com>
2022-11-08 13:02:17 +0530

    Update application.py

--- application.py
+++ application.py
@@ -31,7 +31,7 @@ class Upload(Resource):
         response = {
             'message': 'This is Demo',
             'status': 1,
-            'data': request.json
+            'data': request.form['to_format']
         }
         return response
       
