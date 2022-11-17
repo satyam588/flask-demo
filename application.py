d0be55f2e5e589b9d8914ebf1300d7e64116c354
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-11-17 22:22:39 +0530

    Updated for PDF Split

--- application.py
+++ application.py
@@ -4,6 +4,8 @@ import os
 import random
 import string
 from PIL import Image
+from PyPDF2 import PdfFileReader, PdfFileWriter
+import shutil
 
 application = Flask(__name__, static_url_path='/uploads',
                     static_folder='uploads')
@@ -97,10 +99,110 @@ class Upload(Resource):
             return response
 
 
+class SplitPdf(Resource):
+    def post(self):
+        file = request.files["pdf"]
+
+        if file.filename != '':
+            fileExtention = file.filename.split('.')
+            fileExtention = fileExtention[-1]
+
+            # Save Path
+            savePath = "uploads/pdfs/" + \
+                ''.join(random.choice(string.ascii_lowercase + string.digits)
+                        for _ in range(10))+"."+fileExtention
+            file.save(savePath)
+            # Get Size in byte
+            size = os.stat(savePath).st_size
+
+            # Get Mime-type
+            allowedImageType = ['pdf']
+
+            if (size < 5000000) and (fileExtention in allowedImageType):
+                # try:
+                uploadedFilename = ''.join(random.choice(string.ascii_lowercase + string.digits)
+                                            for _ in range(10))
+                convertedPath = "uploads/pdfs/splitted/"+uploadedFilename
+
+                inputpdf = PdfFileReader(open(savePath, "rb"))
+
+                if not os.path.exists(convertedPath):
+                    os.makedirs(convertedPath)
+
+                for i in range(inputpdf.numPages):
+                    output = PdfFileWriter()
+                    if (request.form['extract'] == 'page') and (request.form['page'] != ''):
+                        pageList = request.form['page'].split(',')
+                        if str(i + 1) in pageList:
+                            i = i + 1
+                        if str(i) in pageList:
+                            print(i)
+                            output.addPage(inputpdf.getPage(i-1))
+                            with open(convertedPath+"/"+uploadedFilename+"_page_"+str(i)+".pdf", "wb") as outputStream:
+                                output.write(outputStream)
+
+                    elif (request.form['extract'] == 'range') and (request.form['range'] != ''):
+                        pageRange = request.form['range'].split('-')
+                        
+                        if int(pageRange[1])== inputpdf.numPages:
+                            i = i + 1
+
+                        if (i >= int(pageRange[0])) and (i <= int(pageRange[1])):
+                            output.addPage(inputpdf.getPage(i-1))
+                            with open(convertedPath+"/"+uploadedFilename+"_page_"+str(i)+".pdf", "wb") as outputStream:
+                                output.write(outputStream)
+
+                    else:
+                        output.addPage(inputpdf.getPage(i))
+                        with open(convertedPath+"/"+uploadedFilename+"_page_"+str(i+1)+".pdf", "wb") as outputStream:
+                            output.write(outputStream)
+
+                shutil.make_archive(
+                    "uploads/pdfs/splitted/"+uploadedFilename, "zip", convertedPath)
+                shutil.rmtree(convertedPath, ignore_errors=True)
+                message = 'Conversion Success!'
+                data = {
+                    'filename': file.filename,
+                    'size': size,
+                    'from_format': fileExtention,
+                    'save_path': savePath,
+                    'converted_filename': uploadedFilename,
+                    'zip_file': convertedPath+'.zip'
+                }
+                # except:
+                #     message = 'Something went Wrong, Please try again!'
+                #     data = []
+
+                response = {
+                    'message': message,
+                    'status': 1,
+                    'data': data
+                }
+
+                return response
+            else:
+                if os.path.exists(savePath):
+                    os.remove(savePath)
+                response = {
+                    'message': 'Only PDF file and Less then 2MB allowed!',
+                    'status': 0,
+                    'data': []
+                }
+                return response
+        else:
+            response = {
+                'message': 'Pdf file is Required!',
+                'status': 0,
+                'data': []
+            }
+            return response
+
+
 # Routes
 api.add_resource(Index, '/')
 api.add_resource(Param, '/param/<int:num>/<int:num2>')
 api.add_resource(Upload, '/upload')
+api.add_resource(SplitPdf, '/split-pdf')
 
 
 if __name__ == "__main__":
