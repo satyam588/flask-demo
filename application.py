0820ac85694e6b24aa36b2e76686394fb218e942
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-11-26 02:32:50 +0530

    Added Merger PDF Class

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import os
 import random
 import string
 from PIL import Image
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfFileReader, PdfFileWriter, PdfMerger
 import shutil
 
 application = Flask(__name__, static_url_path='/uploads',
@@ -121,7 +121,7 @@ class SplitPdf(Resource):
             if (size < 5000000) and (fileExtention in allowedImageType):
                 # try:
                 uploadedFilename = ''.join(random.choice(string.ascii_lowercase + string.digits)
-                                            for _ in range(10))
+                                           for _ in range(10))
                 convertedPath = "uploads/pdfs/splitted/"+uploadedFilename
 
                 inputpdf = PdfFileReader(open(savePath, "rb"))
@@ -143,8 +143,8 @@ class SplitPdf(Resource):
 
                     elif (request.form['extract'] == 'range') and (request.form['range'] != ''):
                         pageRange = request.form['range'].split('-')
-                        
-                        if int(pageRange[1])== inputpdf.numPages:
+
+                        if int(pageRange[1]) == inputpdf.numPages:
                             i = i + 1
 
                         if (i >= int(pageRange[0])) and (i <= int(pageRange[1])):
@@ -198,11 +198,99 @@ class SplitPdf(Resource):
             return response
 
 
+class MergePdf(Resource):
+    def post(self):
+        files = request.files.getlist("pdfs")
+
+        folderName = ''.join(random.choice(string.ascii_lowercase + string.digits)
+                             for _ in range(10))
+        uploadLocation = "uploads/pdfs/merge/"+folderName
+        if not os.path.exists(uploadLocation):
+            os.makedirs(uploadLocation)
+        # savePath = "uploads/" + \
+        #     ''.join(random.choice(string.ascii_lowercase + string.digits)
+        #             for _ in range(10))+"."+fileExtention
+        count = 1
+        allFileName = []
+
+        # return uploadLocation
+        for file in files:
+            if file.filename != '':
+                fileExtention = file.filename.split('.')
+                fileExtention = fileExtention[-1]
+                savePath = uploadLocation+"/"+str(count)+"."+fileExtention
+                file.save(savePath)
+                # Get Size in byte
+                size = os.stat(savePath).st_size
+
+                # Get Mime-type
+                allowedImageType = ['pdf']
+
+                if (size < 5000000) and (fileExtention in allowedImageType):
+                    allFileName.append(str(count)+"."+fileExtention)
+                    # try:
+                    uploadedFilename = ''.join(random.choice(string.ascii_lowercase + string.digits)
+                                               for _ in range(10))
+                    convertedPath = "uploads/pdfs/splitted/"+uploadedFilename
+
+                    message = 'Merged Success!'
+                    data = {
+                        'filename': file.filename,
+                        'size': size,
+                        'from_format': fileExtention,
+                        'save_path': savePath,
+                        'converted_filename': uploadedFilename,
+                    }
+
+                    response = {
+                        'message': message,
+                        'status': 1,
+                        'data': data
+                    }
+
+                else:
+                    if os.path.exists(savePath):
+                        os.remove(savePath)
+                    response = {
+                        'message': 'Only PDF file and Less then 2MB allowed!',
+                        'status': 0,
+                        'data': []
+                    }
+                    return response
+            else:
+                response = {
+                    'message': 'Pdf files are Required!',
+                    'status': 0,
+                    'data': []
+                }
+                return response
+            count = count + 1
+
+        merger = PdfMerger()
+
+        for pdf in allFileName:
+            merger.append(uploadLocation+"/"+pdf)
+
+        resultPdf = "uploads/pdfs/merge/result/"+folderName+".pdf"
+        merger.write(resultPdf)
+        merger.close()
+
+        response = {
+            'message': message,
+            'status': 1,
+            'folder_Name': uploadLocation,
+            'file_Names': allFileName,
+            'result_Pdf': resultPdf,
+        }
+        return response
+
+
 # Routes
 api.add_resource(Index, '/')
 api.add_resource(Param, '/param/<int:num>/<int:num2>')
 api.add_resource(Upload, '/upload')
 api.add_resource(SplitPdf, '/split-pdf')
+api.add_resource(MergePdf, '/merge-pdf')
 
 
 if __name__ == "__main__":
