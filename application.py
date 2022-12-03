8d8485fc9c9f439d69669cf550ae56c9228a5bb2
Satyam Sundaram <sundaramsatyam12@gmail.com>
2022-12-04 00:33:52 +0530

    Update application.py

--- application.py
+++ application.py
@@ -306,9 +306,10 @@ class PdfToImage(Resource):
             allowedImageType = ['pdf']
 
             if (size < 5000000) and (fileExtention in allowedImageType):
+                return file.filename
                 # try:
                 uploadedFilename = ''.join(random.choice(string.ascii_lowercase + string.digits)
-                                        for _ in range(10))
+                                           for _ in range(10))
                 convertedPath = "uploads/converted/pdf-to-image/"+uploadedFilename
 
                 if not os.path.exists(convertedPath):
